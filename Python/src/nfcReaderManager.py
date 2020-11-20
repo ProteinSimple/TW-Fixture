109bb02d0e6b02a485ce966328f8add284a52b5e
kjeffrisps <kenny.jeffris@bio-techne.com>
2020-11-20 10:29:06 -0800

    removed python, preparing for submodule re-add

--- Python/src/nfcReaderManager.py
+++ Python/src/nfcReaderManager.py
@@ -10,12 +10,10 @@ from nfc import nfc_comm as nfc_comm
 from nfc import scard_wrapper as scw
 
 
-class nfcReader(object):
+class nfcReaderManager(object):
     reader = None
 
     def __init__(self):
-        self.logger = logging.getLogger('rfid')
-        self.logger.info('Establishing nfcReader connection.')
         self.nfc = nfc_comm.NFC()
         self.__connect()
 
@@ -37,51 +35,10 @@ class nfcReader(object):
                 return
         self.reader = None
 
-    def eraseTag(self):
-        with nfc_comm.Connection(self.nfc, self.reader) as conn:
-            tag = nfc_comm.maybe_tag(conn)
-            if tag:
-                tag.write(conn, [0xA5] * tag.DATA_LEN)
-
-    def getDefaults(self, cartridgeType, customValues):
-        '''
-        Get the default values for a cartridge type.
-        Changes default values based on the customValues key in config.json
-        '''
-        defaults = packet.v2_defaults(cartridgeType)
-        for key in customValues.keys():
-            if isinstance(customValues[key], (list, dict, tuple)):
-                for value in customValues[key].keys():
-                    defaults[key][value] = customValues[key][value]
-            else:
-                if key == 'serial_prefix':
-                    change_char(defaults['serial_number'],
-                                0, str(customValues[key])
-                                )
-                else:
-                    defaults[key] = customValues[key]
-        return defaults
-
     def getTag(self):
         '''Return a tag if it is available'''
         with nfc_comm.Connection(self.nfc, self.reader) as conn:
             return nfc_comm.maybe_tag(conn)
-
-    def isAvailable(self):
-        return self.reader is not None
-
-    def isFormatted(self, data):
-        '''Return tag formatted status'''
-        if data:
-            return data['formatted']
-        else:
-            return False
-
-    def hasHistory(self, data):
-        if self.isFormatted(data):
-            return len(data['fields']['batch_log']) > 0
-        else:
-            return False
 
     def read(self):
         '''Obtain Tag Data.  Returns None if no data is present'''
@@ -112,10 +69,3 @@ class nfcReader(object):
         else:
             return False
 
-    def write(self, data):
-        self.reconnect()
-        with nfc_comm.Connection(self.nfc, self.reader) as conn:
-            tag = nfc_comm.maybe_tag(conn)
-            if tag:
-                encodedData = packet.encode(data)
-                tag.write(conn, encodedData)
