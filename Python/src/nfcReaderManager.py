e199c98eeae9f6e86b998780d6ffa74cd65673fb
kjeffrisps <kenny.jeffris@bio-techne.com>
2020-11-23 16:49:27 -0800

    fixed object behavior

--- Python/src/nfcReaderManager.py
+++ Python/src/nfcReaderManager.py
@@ -7,10 +7,6 @@ from nfc import scard_wrapper as scw
 
 
 class nfcReaderManager(object):  
-    def __init__(self):
-        self.nfc = nfc_comm.NFC()
-
-
     def close(self):
         self.nfc.close()
 
@@ -19,38 +15,33 @@ class nfcReaderManager(object):
         self.close()
 
 
-    def __enter__(self):
-        self.readers = self.nfc.get_readers()
-
-
-    def __exit__(self, type_, value, traceback):
-        self.close()
-    
-
-    def decode_value(value):
-        return ''.join(chr(i) for i in value)
-
-
-    def get_attrib(hcard, dwAttrId):
-        rv, attrib = sc.SCardGetAttrib(hcard, dwAttrId)
-        if rv == sc.SCARD_S_SUCCESS:
-            return decode_value(attrib)
-        else:
-            raise scw.NFCException(rv)
-
-
     def read_card_with_serial(self, serial):
-        for reader in self.readers:
+        self.nfc = nfc_comm.NFC()
+        readers = self.nfc.get_readers()
+        for reader in readers:
             if 'SCM Microsystems' in reader:
                 try:
                     with nfc_comm.Connection(self.nfc, reader) as conn:
                         currentSerial = get_attrib(conn.hcard, sc.SCARD_ATTR_VENDOR_IFD_SERIAL_NO)
                         if currentSerial == serial:
                             tag = nfc_comm.maybe_tag(conn)
-                            if tag is none:
+                            if tag is None:
                                 return('Cartridge not detected.')
                             else:
                                 return(str(tag.dic))
                 except scw.NFCException as nfce:
-                    continue
+                    if nfce.err_code == -2146434967:
+                        continue
+                    elif nfce.err_code == -2146435056:
+                        return('Please re-insert the cartridge')
         return('The specified reader was not detected.')
+
+def decode_value(value):
+    return ''.join(chr(i) for i in value)
+
+def get_attrib(hcard, dwAttrId):
+    rv, attrib = sc.SCardGetAttrib(hcard, dwAttrId)
+    if rv == sc.SCARD_S_SUCCESS:
+        return decode_value(attrib)
+    else:
+        raise scw.NFCException(rv)
--- Python/src/read_card_with_serial.py
+++ Python/src/read_card_with_serial.py
@@ -1,12 +1,13 @@
+import sys
 from nfcReaderManager import nfcReaderManager
 
 def main(serial): 
     if isinstance(serial, str):
         # Return if the index value is greater than the number of available readers
         try:
-            with nfcReaderManager() as mgr:
-                data = mgr.read_card_with_serial(serial)
-        except:
+            nfcManager = nfcReaderManager()
+            data = nfcManager.read_card_with_serial(serial)
+        except Exception as err:
             return('No NFC readers detected.')
     else:
         return 'The Serial Number Input should be a String'
